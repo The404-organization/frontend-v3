b8a6cd0d523997a641ec70efe5d0401b5b280f0c
Ivan Poliarush <sashapolarush@gmail.com>
2024-11-28 15:17:40 +0200

    feat: add mission section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { MissionSection } from '@/sections/mission-section/mission-section';
 import { WelcomeSection } from '@/sections/welcome-section/welcome-section';
 
 const HomePage = () => {
 	return (
 		<main>
 			<WelcomeSection />
+			<MissionSection />
 		</main>
 	);
 };
---
+++ src/sections/mission-section/mission-section.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@/shared/ui/button/button';
+import { quantumFont } from '@/shared/ui/fonts';
+import clsx from 'clsx';
+import styles from './mission-section.module.css';
+
+export const MissionSection = () => {
+	return (
+		<section className={styles.wrapper}>
+			<div className={styles.blur} />
+			<img src="/common/circle.png" className={styles.largeCircle} />
+			<img src="/common/circle.png" className={styles.smallCircle} />
+			<div className={styles.content}>
+				<div className={styles.mainContent}>
+					<p className={clsx(styles.title, quantumFont.className)}>
+						Mission
+					</p>
+					<div className={styles.textContent}>
+						<div className={styles.textWrapper}>
+							<p className={styles.text}>
+								The404.io is dedicated to transforming illiquid
+								NFTs into tradable, valuable assets by
+								leveraging the power of the ERC-404 token
+								standard and a unique liquidity mechanism.
+							</p>
+							<p className={styles.text}>
+								Our platform allows users to fractionalize their
+								NFTS and participate in a vibrant market,
+								enhancing liquidity and providing new
+								opportunities for earning and trading.
+							</p>
+						</div>
+						<Button
+							wrapperClassName={styles.button}
+							label="White paper"
+						/>
+					</div>
+				</div>
+				<img
+					className={styles.god}
+					src="/mission-section/god.png"
+					alt=""
+				/>
+			</div>
+		</section>
+	);
+};
