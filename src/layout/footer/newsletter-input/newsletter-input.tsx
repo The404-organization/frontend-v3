b63e021edced0290e555abbac7c7a91bdb9b5c1c
Ivan Poliarush <sashapolarush@gmail.com>
2024-12-06 14:54:38 +0200

    feat: connect email newsletter to backend

--- src/layout/footer/newsletter-input/newsletter-input.tsx
+++ src/layout/footer/newsletter-input/newsletter-input.tsx
@@ -1,13 +1,62 @@
+'use client';
+
 import { SubscribeIcon } from '@/assets/icons/subscribe-icon';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
 import styles from './newsletter-input.module.css';
 
+interface Values {
+	email: string;
+}
+
+const validationSchema = Yup.object().shape({
+	email: Yup.string().email().required(),
+});
+
 export const NewsletterInput = () => {
+	const handleSubmit = async (
+		values: Values,
+		formikHelpers: FormikHelpers<Values>,
+	) => {
+		try {
+			await fetch('/api/submit-form', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					email: values.email,
+				}),
+			});
+		} catch (error) {
+			console.log('Failed to submit form:', error);
+		} finally {
+			formikHelpers.resetForm();
+		}
+	};
+
 	return (
-		<div className={styles.wrapper}>
-			<input placeholder="Your Email" className={styles.input} />
-			<button className={styles.subscribeButton}>
-				<SubscribeIcon />
-			</button>
-		</div>
+		<Formik
+			initialValues={{ email: '' }}
+			validationSchema={validationSchema}
+			onSubmit={handleSubmit}>
+			{({ isSubmitting }) => (
+				<Form className={styles.wrapper}>
+					<Field
+						id="email"
+						name="email"
+						placeholder="Your Email"
+						className={styles.input}
+					/>
+					<button className={styles.subscribeButton} type="submit">
+						{isSubmitting ? (
+							<div className={styles.loader} />
+						) : (
+							<SubscribeIcon />
+						)}
+					</button>
+				</Form>
+			)}
+		</Formik>
 	);
 };
