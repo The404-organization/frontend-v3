8d2fd96d1fae9ab8fce8bad41eea7715f323c834
Ivan Poliarush <sashapolarush@gmail.com>
2024-12-06 14:31:23 +0200

    fix: input and textarea fonts

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,8 +1,14 @@
 import clsx from 'clsx';
 import { Field } from 'formik';
+import { outfitFont } from '../fonts';
 import styles from './input.module.css';
 import { InputProps } from './input.props';
 
 export const Input = ({ className, ...props }: InputProps) => {
-	return <Field className={clsx(styles.input, className)} {...props} />;
+	return (
+		<Field
+			className={clsx(styles.input, outfitFont.className, className)}
+			{...props}
+		/>
+	);
 };
--- src/shared/ui/textarea/textarea.tsx
+++ src/shared/ui/textarea/textarea.tsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx';
 import { Field } from 'formik';
+import { outfitFont } from '../fonts';
 import styles from './textarea.module.css';
 import { TextareaProps } from './textarea.props';
 
 export const Textarea = ({ className, ...props }: TextareaProps) => {
 	return (
 		<Field
-			className={clsx(styles.textarea, className)}
+			className={clsx(styles.textarea, outfitFont.className, className)}
 			as="textarea"
 			{...props}
 		/>
