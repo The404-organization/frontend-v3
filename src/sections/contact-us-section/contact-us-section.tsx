69af577cc073d444fb7fbefd362c4e1f3a4108b1
Ivan Poliarush <sashapolarush@gmail.com>
2024-12-10 22:25:14 +0200

    feat: update contacts

--- src/sections/contact-us-section/contact-us-section.tsx
+++ src/sections/contact-us-section/contact-us-section.tsx
@@ -1,7 +1,6 @@
 import { ArrowsIcon } from '@/assets/icons/arrows-icon';
 import { LocationIcon } from '@/assets/icons/location-icon';
 import { MainIcon } from '@/assets/icons/main-icon';
-import { PhoneIcon } from '@/assets/icons/phone-icon';
 import { poppinsFont } from '@/shared/ui/fonts';
 import clsx from 'clsx';
 import { ContactUsForm } from './contact-us-form/contact-us-form';
@@ -17,16 +16,7 @@ export const ContactUsSection = () => {
 				<div className={styles.schemaItemBlock}>
 					<MainIcon className={styles.schemaIcon} />
 					<div>
-						<p className={styles.schemaText}>company@gmail.com</p>
-						<p className={styles.schemaText}>infoweb@gmail.com</p>
-					</div>
-				</div>
-				<ArrowsIcon className={styles.arrowIcon} />
-				<div className={styles.schemaItemBlock}>
-					<PhoneIcon className={styles.schemaIcon} />
-					<div>
-						<p className={styles.schemaText}>+84 0977425031</p>
-						<p className={styles.schemaText}>+998 765 775 34</p>
+						<p className={styles.schemaText}>admin@the404.io</p>
 					</div>
 				</div>
 				<ArrowsIcon className={styles.arrowIcon} />
@@ -36,7 +26,7 @@ export const ContactUsSection = () => {
 						<p className={styles.schemaText}>
 							State/province/area:
 						</p>
-						<p className={styles.schemaText}>Georgia 198</p>
+						<p className={styles.schemaText}>Brooklyn NY</p>
 					</div>
 				</div>
 			</div>
